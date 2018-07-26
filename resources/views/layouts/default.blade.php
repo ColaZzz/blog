08e2d4b7469644343c314bb959e5eafdfeca2f07
Cola <ljlhyyc@qq.com>
2018-07-26 13:29:20 +0000

    Add user statuses

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Blog') - Blog</title>
+    <title>@yield('title','Blog') - 记录你的生活</title>
 
     <link rel="stylesheet" href="/css/app.css">
 </head>
